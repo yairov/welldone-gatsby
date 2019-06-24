e0a3ffa40341dcbab0fdc172227362d7a10a8663
Dani Kenan <danikenan@gmail.com>
2019-06-24 08:18:47 +0300

    deployment

---
+++ deployment/deploy.sh
@@ -0,0 +1,15 @@
+
+pushd ..
+
+yarn build
+
+popd
+
+! kubectl delete -f gke.yaml
+
+docker build -t gcr.io/welldone-deployments/welldone-gatsby ..
+
+docker push gcr.io/welldone-deployments/welldone-gatsby
+
+kubectl apply -f gke.yaml
+
