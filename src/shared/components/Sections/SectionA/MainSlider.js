30a1ea7be76b760f957c70f8249d7dd705ff9aaf
TalYehoshua <talyeh3@gmail.com>
2019-06-25 17:19:59 +0300

    Change design of header, Triangles,and play button

--- src/shared/components/Sections/SectionA/MainSlider.js
+++ src/shared/components/Sections/SectionA/MainSlider.js
@@ -13,12 +13,13 @@ import SlideView from './SlideView/SlideView';
 
 
 const BackgroundTriangle = styled.img.attrs({src: triangle})`
+
+height: 60rem;
 position: absolute;
-right: -15rem;
-top: -5rem;
+right: -24rem;
+top: -6rem;
 width: 90rem;
 z-index: -1;
-height: 50rem;
 `;
 
 const LeftTopTriangle = styled.img.attrs({
@@ -27,7 +28,7 @@ src: lefttopbg,
 
 position: absolute;
 left: -16rem;
-top: -5rem;
+top: -10rem;
 z-index: -1;
 width: 40rem;
 transform: rotate(-202deg);
@@ -38,11 +39,11 @@ const LeftBottomTriangle = styled.img.attrs({src: leftbottombg})`
 
 position: absolute;
 left: -7rem;
-top: 25rem;
+top: 24rem;
 z-index: -1;
 width: 20rem;
 transform: rotate(143deg);
-height: 20rem;
+height: 24rem;
 `;
 
 const Content = styled.div`
@@ -54,15 +55,15 @@ padding-top: 0rem;
 & > div {
   ${media.minSmallDesktop`
     padding: 0 10rem;
-    
+    padding-right: 0;
   `}
 }
 `;
 
 const Size = styled.div`
-    height: 60rem;
+    height: 65rem;
     ${media.minSmallDesktop`
-        height: 45rem;
+        height: 50rem;
         text-align: center;
     `}
 `;
